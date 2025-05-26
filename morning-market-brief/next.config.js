a05dfceb13f45f60850b14fd6dc5a0d8920d861e
karthikpk20046 <164026979+karthikpk20046@users.noreply.github.com>
2025-05-27 02:43:44 +0530

    Update next.config.js

--- morning-market-brief/next.config.js
+++ morning-market-brief/next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net;
+  style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;
+  font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+  img-src 'self' data: https:;
+  connect-src 'self' https:;
+  object-src 'none';
+  base-uri 'self';
+`;
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   images: { unoptimized: true },
@@ -18,6 +26,19 @@ const nextConfig = {
     "*.macaly-app.com",
     "*.macaly-user-data.dev",
   ],
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
